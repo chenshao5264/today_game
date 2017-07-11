54129f2c84b86ae6a32a3de09943cb6a5ea74de7
chenb <chenb@91game.top>
2017-07-11 21:33:36 +0800

    删除编译出的js文件

--- today_server/build/utils/utility.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.address2ip = function (address) {
-    if (address.indexOf('::ffff:') != -1) {
-        address = address.substr(7);
-    }
-    return address;
-};
